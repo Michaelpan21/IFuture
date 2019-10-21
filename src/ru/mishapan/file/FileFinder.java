3bc660f2bcc8ea3ff25e054b21d5e8c3702dfbaa
Misha <mishaaaaa21@gmail.com>
2019-10-21 17:49:39 +0300

    New: Find folders by name

--- src/ru/mishapan/file/FileFinder.java
+++ src/ru/mishapan/file/FileFinder.java
@@ -3,40 +3,33 @@ package ru.mishapan.file;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /*TODO
-1) Написать класс, перебирающий файлы в папке
-2) Добавить возможно задавать расширение файлов
+1) кодировка
  */
 public class FileFinder {
 
     public List<Path> findFiles(Path path, String glob) {
 
-        List<Path> pathList = new ArrayList<>();
+        List<Path> filesPathList = new ArrayList<>();
 
         try {
             Iterable<Path> stream = Files.newDirectoryStream(path, glob);
-            stream.forEach(pathList::add);
+            stream.forEach(filesPathList::add);
 
         } catch (IOException | DirectoryIteratorException ex) {
             ex.printStackTrace();
         }
 
-        return pathList;
+        return filesPathList;
     }
 
-    /*
-    Прочитать строку, если найдено частичное совпадение, проверить продолжение в другой строке
-     */
     public List<Path> findTextInFiles(List<Path> pathList, String text) {
 
         if (pathList.size() == 0) throw new IllegalArgumentException("No files to search");
@@ -102,10 +95,10 @@ public class FileFinder {
         FileFinder files = new FileFinder();
         Path path = FileSystems.getDefault().getPath("C:/Users/Михаил/Desktop/findMe");
 
-        Pattern pattern = Pattern.compile("Error[\\s\n]404:[\\s\n]Server[\\s\n]not[\\s\n]found");
-        //Pattern pattern1 = Pattern.compile("人去买酒");
         String text = "Error 404: Server not found";
-        files.findTextInFiles(files.findFiles(path, "*.log"), text);
+        List<Path> st = files.findTextInFiles(files.findFiles(path, "*.*"), text);
+
+        st.forEach(System.out::println);
 
     }
 
